2cfdca26f1ca958280c35eb689fcc98da921b0f2
Simon FLORENTIN <sifl@4js.com>
2020-04-14 09:57:40 +0200

    Clean and better structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # thebard-discordbot
 Discord bot for RPG Game Masters
+
+# Commands
+
+List of available commands
+- `/r initiative` to roll 1D20 for initiative
+- `/r <atk> <def>` to roll <atk>D6 for attack and <def>D6 for defense
+
+
+
+# Installation
+
+You will need to install icecast + darkcast to stream audio from your computer to the bot 
--- thebard.js
+++ thebard.js
@@ -1,23 +1,23 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const Canvas = require('canvas');
 
-const bot = new Discord.Client()
+const bot = new Discord.Client();
 
 bot.on('ready', function () {
-  console.log("Je suis connecté !");
-/*  bot.user.setAvatar("./bard2.png")
+	console.log("Je suis connecté !");
+/*  bot.user.setAvatar("./assets/bard2.png")
     .then(() => console.log("avatar OK"))
     .catch(console.error)
   */
-})
+});
 
-bot.login('Njk4OTI2NDYyNDQ2NjAwMjMy.XpM-DQ.j-DnV5XT7I7nwC7ny6U6VPdf9QE')
+bot.login('Njk4OTI2NDYyNDQ2NjAwMjMy.XpM-DQ.j-DnV5XT7I7nwC7ny6U6VPdf9QE');
 
 bot.on('message', message => {
   if (message.content === 'ping') {
     message.reply('pong !')
   }
-})
+});
 
 /*bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
@@ -31,24 +31,22 @@ bot.on('message', message => {
 bot.on('message', message => {
 
   if (message.content.startsWith('!play')) {
-    // On récupère les arguments de la commande
-    let args = message.content.split(' ')
-    // On rejoint le channel audio
-    const voiceChannel = message.member.voice.channel.join()
-      .then(function (connection) {
-	// From a path
-	//connection.play('audio.mp3');
-	// From a ReadableStream
-	//connection.play(fs.createReadStream('audio.mp3'));
-	// From a URL
-	connection.play('http://10.0.0.42:8000/thebard.ogg');
-        message.channel.send('playing time!')
+		// On récupère les arguments de la commande
+		let args = message.content.split(' ');
+		// On rejoint le channel audio
+		const voiceChannel = message.member.voice.channel.join()
+			.then(function (connection) {
+				// From a path
+				//connection.play('audio.mp3');
+				// From a ReadableStream
+				//connection.play(fs.createReadStream('audio.mp3'));
+				// From a URL
+				connection.play('http://10.0.0.42:8000/thebard.ogg');
+				message.channel.send('playing time!')
+			})
+		}
 
-
-      })
-  }
-
-})
+});
 
 // Pass the entire Canvas object because you'll need to access its width, as well its context
 const applyText = (canvas, text) => {
@@ -72,52 +70,52 @@ const applyText = (canvas, text) => {
 
 bot.on('message', async message => {
   if (message.content === '/r initiative') {
-	const channel = message.channel;
-	if (!channel) return;
 
-	const canvas = Canvas.createCanvas(500, 110);
-	const ctx = canvas.getContext('2d');
+		const channel = message.channel;
+		if (!channel) return;
 
-	//const background = await Canvas.loadImage('./bg-parchemin.jpg');
-	//ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+		const canvas = Canvas.createCanvas(500, 110);
+		const ctx = canvas.getContext('2d');
 
-	//ctx.strokeStyle = '#74037b';
-	//ctx.strokeRect(0, 0, canvas.width, canvas.height);
+		//const background = await Canvas.loadImage('./bg-parchemin.jpg');
+		//ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-	// Slightly smaller text placed above the member's display name
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
+		//ctx.strokeStyle = '#74037b';
+		//ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-	// Add an exclamation point here and below
-	ctx.font = applyText(canvas, `${message.member.displayName}`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${message.member.displayName}`, canvas.width / 2.5, canvas.height / 1.8);
+		// Slightly smaller text placed above the member's display name
+		ctx.font = '28px sans-serif';
+		ctx.fillStyle = '#ffffff';
 
-	let initiative = Math.floor(20*Math.random())+1;
+		// Add an exclamation point here and below
+		ctx.font = applyText(canvas, `${message.member.displayName}`);
+		ctx.fillStyle = '#ffffff';
+		ctx.fillText(`${message.member.displayName}`, canvas.width / 2.5, canvas.height / 1.8);
 
-	const avatar = await Canvas.loadImage('./dice/d20.png');
-	ctx.drawImage(avatar, 5, 5, 100, 100);
-        ctx.font = '19px sans-serif';
-        ctx.textAlign = "center"
-	ctx.fillText(`${initiative}`, 56, canvas.height / 1.75);
+		let initiative = Math.floor(20*Math.random())+1;
 
-	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'bard.png');
-	channel.send(`Initiative pour ${message.member}: ${initiative}`, attachment);
+		const avatar = await Canvas.loadImage('./dice/d20.png');
+		ctx.drawImage(avatar, 5, 5, 100, 100);
+				ctx.font = '19px sans-serif';
+				ctx.textAlign = "center"
+		ctx.fillText(`${initiative}`, 56, canvas.height / 1.75);
 
-  }
+		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), './assets/bard.png');
+		channel.send(`Initiative pour ${message.member}: ${initiative}`, attachment);
+		console.log(`${message.member.displayName} has rolled for initiative: ${initiative}`);
+	}
 	else if(message.content.match(/\/r \d \d/g)){
 		const channel = message.channel;
 		if (!channel) return;
 
 		let reg = /\/r (\d) (\d)/g;
-		let match = reg.exec(message.content)
+		let match = reg.exec(message.content);
 
 		let atkNumber = parseInt(match[1], 10);
 		let defNumber = parseInt(match[2], 10);
 
 		let atkArray = Array.from({ length: atkNumber }, (_, i) => Math.floor(6*Math.random())+1);
 		let defArray = Array.from({ length: defNumber }, (_, i) => Math.floor(6*Math.random())+1);
-
 
 		channel.send(`Jet pour ${message.member}: ${atkArray} / ${defArray}`);
 
